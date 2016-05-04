3d5128ef51dc6c09955fea96757c3475f22a24ab
Ghiwook Nam <namghiwook@gmail.com>
2016-05-04 14:56:18 +0900

    ..

--- src/main/java/com/namghiwook/mirror/dust/YellowDustController.java
+++ src/main/java/com/namghiwook/mirror/dust/YellowDustController.java
@@ -2,6 +2,7 @@ package com.namghiwook.mirror.dust;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,11 +17,13 @@ import com.squareup.okhttp.Response;
 @RestController 
 public class YellowDustController {
 	
+	Logger logger = Logger.getLogger(getClass());
+	
 	OkHttpClient client = new OkHttpClient();
 	ObjectMapper mapper = new ObjectMapper();
 	
 	@RequestMapping(value = "/dust", method = RequestMethod.GET)
-	public String getYellowDustByDDDCode(@RequestParam("code") String code) {
+	public String getYellowDustByCode(@RequestParam("code") String code) {
 
 		String density = "0";
 		
@@ -39,6 +42,8 @@ public class YellowDustController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		logger.info(String.format("getYellowDustByCode code %s : %s", code, density));
 		
 		return density;
 	}
